8d7a5a5722c79b30feb3bc5593dd3ea709bdacfb
YohanesWijaya <yohanes2420@gmail.com>
2022-01-05 10:54:44 +0700

    update

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const methodOverride = require("method-override");
 const session = require('express-session');
 const flash = require('connect-flash');
 const cors = require('cors');
-
 //import mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://YohanesWijaya:bwamern@cluster0.zam7q.mongodb.net/db_staycation?retryWrites=true&w=majority', { 
@@ -39,6 +38,7 @@ app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
@@ -48,8 +48,6 @@ app.use('/users', usersRouter);
 // admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
-
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
