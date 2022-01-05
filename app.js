21edc1abd9a85c7782a5696b07e83f5318a4b189
YohanesWijaya <yohanes2420@gmail.com>
2022-01-05 13:14:22 +0700

    update

--- app.js
+++ app.js
@@ -38,11 +38,12 @@ app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
+
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // admin
