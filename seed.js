af4b127e235bb6cf602cefa876749de557e961bd
YohanesWijaya <92308185+YohanesWijaya@users.noreply.github.com>
2022-01-12 00:52:46 +0700

    Update seed.js

--- seed.js
+++ seed.js
@@ -1047,14 +1047,14 @@ var data = [
         nameBank: 'Mandiri',
         nomorRekening: '089898',
         name: 'Yohanes Wijaya',
-        imageUrl: 'images/logo bca.png'
+        imageUrl: 'images/logo mandiri.png'
       },
       {
         _id: mongoose.Types.ObjectId('5e96cbe292b97300fc903323'),
         nameBank: 'BCA',
         nomorRekening: '878678',
         name: 'Yohanes Wijaya',
-        imageUrl: 'images/logo mandiri.png'
+        imageUrl: 'images/logo bca.png'
       }
     ]
   },
